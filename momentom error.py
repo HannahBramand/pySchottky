4cac2390fc22bd745f67b33d74a01f81bfc0570d
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2026-05-11 16:20:22 +0200

    fixing

--- momentom error.py
+++ momentom error.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from lib.P_error import get_momentum_data
 
 f_rev = 1e6                
-num_turns = 20                          
+num_turns = 55
 slip_fac_eta = 0.2
 dp_p = 0.1
 
@@ -13,10 +13,10 @@ signal_amplitude = np.ones(num_turns)
 ideal, shifted = get_momentum_data(f_rev, num_turns, slip_fac_eta, dp_p)
 
 #--------------------------------
-plt.stem(pass_times *1e6, signal_amplitude, linefmt='r-', label="ideal particle")
-plt.plot(pass_times *1e6, signal_amplitude ,color='red' , linestyle='--' , alpha=0.5)
-plt.stem(pass_times2 *1e6, signal_amplitude, linefmt='b-', label=r"off-momentum particle $\delta$="+"%g [x10$^{-3}$]"%(momentom_error*1E3))
-plt.plot(pass_times2 *1e6, signal_amplitude ,color='blue' , linestyle='--' , alpha=0.5)
+plt.stem(ideal *1e6, signal_amplitude, linefmt='r-', label="ideal particle")
+plt.plot(ideal *1e6, signal_amplitude ,color='red' , linestyle='--' , alpha=0.5)
+plt.stem(shifted *1e6, signal_amplitude, linefmt='b-', label=r"off-momentum particle $\delta$="+"%g [x10$^{-3}$]"%(dp_p*1E3))
+plt.plot(shifted *1e6, signal_amplitude ,color='blue' , linestyle='--' , alpha=0.5)
 
 plt.title("Single Particle ")
 plt.grid(True)
