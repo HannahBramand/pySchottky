406f4f80549114d78a9cce11c788d558acdc2d6c
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2026-05-11 16:32:41 +0200

    going multi-particle

--- lib/P_error.py
+++ lib/P_error.py
@@ -2,9 +2,7 @@ import numpy as np
 
 def get_momentum_data(f_rev, num_turns, slip_fac_eta, dp_p):
     T_rev = 1.0 / f_rev
-    T_momentum = T_rev * (1 + (slip_fac_eta * dp_p))
+    T_momenta = [T_rev * (1 + (slip_fac_eta * myDp_p)) for myDp_p in dp_p]
+    pass_times = [ np.arange(1, num_turns + 1) * T_momentum for T_momentum in T_momenta ]
     
-    pass_times = np.arange(1, num_turns + 1) * T_rev
-    pass_times2 = np.arange(1, num_turns + 1) * T_momentum
-    
-    return pass_times, pass_times2
+    return pass_times
--- momentom error.py
+++ momentom error.py
@@ -5,18 +5,22 @@ from lib.P_error import get_momentum_data
 f_rev = 1e6                
 num_turns = 55
 slip_fac_eta = 0.2
-dp_p = 0.1
+dp_p = [0.0,0.1,-0.1]
+myColors=["r","b","g","c","m","k"]
 
 
 signal_amplitude = np.ones(num_turns)
 
-ideal, shifted = get_momentum_data(f_rev, num_turns, slip_fac_eta, dp_p)
+pass_times = get_momentum_data(f_rev, num_turns, slip_fac_eta, dp_p)
 
 #--------------------------------
-plt.stem(ideal *1e6, signal_amplitude, linefmt='r-', label="ideal particle")
-plt.plot(ideal *1e6, signal_amplitude ,color='red' , linestyle='--' , alpha=0.5)
-plt.stem(shifted *1e6, signal_amplitude, linefmt='b-', label=r"off-momentum particle $\delta$="+"%g [x10$^{-3}$]"%(dp_p*1E3))
-plt.plot(shifted *1e6, signal_amplitude ,color='blue' , linestyle='--' , alpha=0.5)
+for ii in range(len(pass_times)):
+    plt.stem(pass_times[ii]*1e6, signal_amplitude, linefmt="%s-"%(myColors[ii]))
+    plt.plot(pass_times[ii]*1e6, signal_amplitude, color=myColors[ii])
+# plt.stem(ideal *1e6, signal_amplitude, linefmt='r-', label="ideal particle")
+# plt.plot(ideal *1e6, signal_amplitude ,color='red' , linestyle='--' , alpha=0.5)
+# plt.stem(shifted *1e6, signal_amplitude, linefmt='b-', label=r"off-momentum particle $\delta$="+"%g [x10$^{-3}$]"%(dp_p*1E3))
+# plt.plot(shifted *1e6, signal_amplitude ,color='blue' , linestyle='--' , alpha=0.5)
 
 plt.title("Single Particle ")
 plt.grid(True)
