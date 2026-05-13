ec035b7db99611162fb1d9c89aa7a6122ec232dd
HannahBramand <hanieh.berahmandpour01@universitadipavia.it>
2026-05-13 10:47:38 +0200

    small changes

--- momentom error.py
+++ momentom error.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 from lib.P_error import get_momentum_data
 
 f_rev = 1e6                
-num_turns = 55
-slip_fac_eta = 0.2
+num_turns = 128
+slip_fac_eta = 0.2        
 dp_p = [0.0,0.1,-0.1]
 myColors=["r","b","g","c","m","k"]
 
 
-signal_amplitude = np.ones(num_turns)
+signal_amplitude = np.ones(num_turns)      
 
 pass_times = get_momentum_data(f_rev, num_turns, slip_fac_eta, dp_p)
 
@@ -22,8 +22,8 @@ for ii in range(len(pass_times)):
 # plt.stem(shifted *1e6, signal_amplitude, linefmt='b-', label=r"off-momentum particle $\delta$="+"%g [x10$^{-3}$]"%(dp_p*1E3))
 # plt.plot(shifted *1e6, signal_amplitude ,color='blue' , linestyle='--' , alpha=0.5)
 
-plt.title("Single Particle ")
-plt.grid(True)
+plt.title("Three individual particles ")
+plt.grid(True, linestyle='--', alpha=0.7)
 plt.xlabel(r"time [$\mu$s]")
 plt.ylabel("Amplitude")
 plt.legend()
